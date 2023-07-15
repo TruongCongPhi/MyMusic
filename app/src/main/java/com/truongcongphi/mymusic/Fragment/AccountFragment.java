94adbd983a3e37d5d7983b478a8ad8a4fb3c2c81
nam <namhai17102003@gmail.com>
2023-07-15 11:42:32 +0700

    account

--- app/src/main/java/com/truongcongphi/mymusic/Fragment/AccountFragment.java
+++ app/src/main/java/com/truongcongphi/mymusic/Fragment/AccountFragment.java
@@ -2,22 +2,34 @@ package com.truongcongphi.mymusic.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.ContentView;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.truongcongphi.mymusic.R;
 
 public class AccountFragment extends Fragment {
+    ImageButton img_avt, ic_search, ic_add;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+
+        return inflater.inflate(R.layout.fragment_account, container,false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
---
+++ app/src/main/java/com/truongcongphi/mymusic/Fragment/AccountFragment2.java
@@ -0,0 +1,7 @@
+package com.truongcongphi.mymusic.Fragment;
+
+import android.app.Fragment;
+
+public class AccountFragment2 extends Fragment {
+
+}
