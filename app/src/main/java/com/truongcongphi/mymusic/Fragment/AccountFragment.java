dcb7cb398a1ffaf6de357e9c852cf81ae9123516
nam <namhai17102003@gmail.com>
2023-07-16 10:44:48 +0700

    cgn

--- app/src/main/java/com/truongcongphi/mymusic/Fragment/AccountFragment.java
+++ app/src/main/java/com/truongcongphi/mymusic/Fragment/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.truongcongphi.mymusic.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.ContentView;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import com.truongcongphi.mymusic.Activity.MainActivity;
 import com.truongcongphi.mymusic.R;
 
 public class AccountFragment extends Fragment {
@@ -23,13 +25,19 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        img_avt = (ImageButton) view.findViewById(R.id.img_avt);
+        ic_search = (ImageButton)  view.findViewById(R.id.ic_search);
+        ic_add = (ImageButton) view.findViewById(R.id.ic_add);
 
-        return inflater.inflate(R.layout.fragment_account, container,false);
-    }
+        img_avt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AccountFragment2.class);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+        return view;
     }
 }
