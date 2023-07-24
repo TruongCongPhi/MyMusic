a465df41b7da9987632a19af39ffb745b162448e
TruongCongPhi <tcp02012003@gmail.com>
2023-07-24 22:00:31 +0700

    phi

--- app/src/main/java/com/truongcongphi/mymusic/Activity/LoginActivity.java
+++ app/src/main/java/com/truongcongphi/mymusic/Activity/LoginActivity.java
@@ -86,28 +86,6 @@ public class LoginActivity extends AppCompatActivity {
                                 mDatabase.child("users").child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
                                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                        if (!dataSnapshot.exists()) {
-                                            // Nếu tài khoản chưa tồn tại trên cơ sở dữ liệu thì lưu lên cơ sở dữ liệu
-                                            mDatabase.child("users").child(uid).child("email").setValue(email);
-                                            mDatabase.child("users").child(uid).child("password").setValue(password);
-                                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-                                            sessionManager.saveUserCredentials(email, password, null, null);
-
-                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                            startActivity(intent);
-                                            finish();
-                                        }
-                                    }
-
-                                    @Override
-                                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                    }
-                                });
-
-                                mDatabase.child("users").child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                                         if (dataSnapshot.exists()) {
                                             String image = dataSnapshot.child("imageUser").getValue(String.class);
                                             String name = dataSnapshot.child("name").getValue(String.class);
--- app/src/main/java/com/truongcongphi/mymusic/Fragment/SearchFragment.java
+++ app/src/main/java/com/truongcongphi/mymusic/Fragment/SearchFragment.java
@@ -1,11 +1,13 @@
 package com.truongcongphi.mymusic.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,6 +30,7 @@ import com.truongcongphi.mymusic.R;
 import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
+    RelativeLayout relativeLayout;
 
     private RecyclerView recyclerView;
     private SearchAdapter adapter;
@@ -36,12 +39,14 @@ public class SearchFragment extends Fragment {
 
     private DatabaseReference databaseRef;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         editTextSearch = view.findViewById(R.id.search_box);
+        relativeLayout = view.findViewById(R.id.rll_tv);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
